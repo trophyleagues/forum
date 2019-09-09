e448bc201279fa3b43738817e7036f0fbebfcd6b
Farid Murzone <faridmurzone@gmail.com>
2019-09-08 22:47:16 -0300

    Added loading component

--- src/components/GeneralComponents/Loading.js
+++ src/components/GeneralComponents/Loading.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
-import { MoonLoader } from 'react-spinners';
+import { ClipLoader } from 'react-spinners';
  
 const Loading = () => {
   const loading = useSelector(state => state.front.loading)
+  const override = `
+  position: absolute;
+  right: 0;
+  left: 0;
+  top: 20%;
+  margin: 0 auto;
+  z-index: 5;
+  `
 
-  return <MoonLoader 
+  return (
+  <div className='sweet-loading'>
+  <ClipLoader 
+    css={override}
     sizeUnit={"px"}
     size={150}
     color={'#65b8f8'}
-    loading={loading} />
-
+    loading={true} />
+    </div>)
 }
 
 export default Loading;
--- src/store/reducers/frontReducers.js
+++ src/store/reducers/frontReducers.js
@@ -1,31 +1,12 @@
-// export function userActions(state = {userAction = "", updatePost: ""}, action) {
-//   switch (action.type) {
-//       case 'USER_CURRENT_ACTION':
-//           return {
-//             userAction: action.payload
-//           };
-//       case 'UPDATE_POST':
-//           return {
-//             updatePost: action.payload
-//           }
-//       default:
-//           return state;
-//   }
-// }
+const initialState = {
+  loading: false
+}
 
-// export function modal(state = {modalType = "", modalOpen = false}) {
-//   switch(action.type) {
-//     case 'OPEN_MODAL':
-//       return {
-//         modalType: action.payload,
-//         modalOpen: true
-//       }
-//     case 'CLOSE_MODAL':
-//       return {
-//         modalType: action.payload,
-//         modalOpen: false
-//       }
-//   }
-// }
-
-
+export default function frontReducers(state = initialState, action) {
+  switch (action.type) {
+      case 'LOADING':
+          return {...state, loading: action.payload};
+      default:
+          return state;
+  }
+}
